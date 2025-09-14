3bb1c0275f4cf81740428642d526b535c540ec40
aopon222 <aopon222@example.com>
2025-09-14 17:34:04 +0700

    initial commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,16 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nomor telepon')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->string('avatar')->nullable();   
+            $table->text('alamat')->nullable();
+            $table->string('kota')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
